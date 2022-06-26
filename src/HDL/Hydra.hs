13fce9edcc09c5c70895c85f687ec97514d4f0ec
John O'Donnell <john.t.odonnell9@gmail.com>
2022-06-26 15:47:06 +0100

    fix conflict edits

--- src/HDL/Hydra.hs
+++ src/HDL/Hydra.hs
@@ -5,11 +5,7 @@ import HDL.Hydra.Circuits.Combinational
 import HDL.Hydra.Circuits.Register
 
 hydraVersion :: String
-<<<<<<< HEAD
 hydraVersion = "3.4.16dev17"
-=======
-hydraVersion = "3.4.15edited-but-not-installed"
->>>>>>> 0f019b698764ae7a30fec5120de54e8f99c591c5
 
 printVersion :: IO ()
 printVersion = putStrLn ("Hydra version " ++ hydraVersion)
